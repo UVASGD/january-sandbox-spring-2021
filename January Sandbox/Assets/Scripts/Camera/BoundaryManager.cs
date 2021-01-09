64107b35e41a70b21a18544e0a503e9dd5ce32eb
Brandon Nielsen <brn5jc@virginia.edu>
2021-01-09 12:48:50 -0500

    Added a lot more to the camera so that it can support a room based system. It currently kinda sucks so its a work in progress.

---
+++ January Sandbox/Assets/Scripts/Camera/BoundaryManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundaryManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject boundary;
+
+    private Transform player;
+    private BoxCollider manageBoundary;
+
+    private void Start()
+    {
+        manageBoundary = GetComponent<BoxCollider>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        activateBoundary();
+    }
+
+    private void activateBoundary()
+    {
+        if(player.position.x > manageBoundary.bounds.min.x && player.position.x < manageBoundary.bounds.max.x &&
+            player.position.z > manageBoundary.bounds.min.z && player.position.z < manageBoundary.bounds.max.z)
+        {
+            boundary.SetActive(true);
+        }
+        else
+        {   
+            boundary.SetActive(false);
+        }
+    }
+}
---
+++ January Sandbox/Assets/Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 cameraOffset;
+
+    private Transform playerPosition;
+    private float lerpTime = .3f;
+    private BoxCollider cameraCollider;
+    private void Start()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        cameraCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        FollowPlayerWithScreens();
+    }
+    
+    private void FollowPlayerWithScreens()
+    {
+        if (GameObject.Find("Boundary"))
+        {
+            Vector3 temp = new Vector3(Mathf.Clamp(playerPosition.position.x, GameObject.Find("Boundary").GetComponent<BoxCollider>().bounds.min.x + cameraCollider.size.x / 2, GameObject.Find("Boundary").GetComponent<BoxCollider>().bounds.max.x - cameraCollider.size.x / 2)
+                                        , playerPosition.position.y
+                                        , Mathf.Clamp(playerPosition.position.z, GameObject.Find("Boundary").GetComponent<BoxCollider>().bounds.min.z + cameraCollider.size.z / 2, GameObject.Find("Boundary").GetComponent<BoxCollider>().bounds.max.z - cameraCollider.size.z / 2));
+            Debug.Log(temp);
+            Vector3 realTemp = temp + cameraOffset;
+            Vector3 nextPosition = Vector3.Lerp(transform.position, realTemp, lerpTime);
+            transform.position = nextPosition;
+        }
+    }
+}
