93b6b1ce3a1dd3b87bdb4f46cd800edd0b33df1e
Brandon Nielsen <brn5jc@virginia.edu>
2021-01-09 12:52:19 -0500

    Added back basic camera for temporary purposes.

---
+++ January Sandbox/Assets/Scripts/Camera/Basic Camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 cameraOffset;
+
+    private Transform playerPosition;
+    private float lerpTime = .3f;
+    private void Start()
+    {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {       
+        Vector3 temp = playerPosition.position + cameraOffset;
+        Vector3 nextPosition = Vector3.Lerp(transform.position, temp, lerpTime);
+        transform.position = nextPosition;
+        
+    }
+}
